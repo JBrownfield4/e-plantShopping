3a012277f78d69e87fd1849ba627c01c70ab5958
JBrownfield4 <justinbrownfield2000@gmail.com>
2024-12-04 11:15:27 -0600

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,23 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
 addItem: (state, action) => {
+  const newItem  = action.payload;
   const { name, image, cost } = action.payload;
   const existingItem = state.items.find(item => item.name === name);
   if (existingItem) {
     existingItem.quantity++;
   } else {
+    newItem.quantity = 1;
     state.items.push({ name, image, cost, quantity: 1 });
   }
 },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        const toBeRemoveItem  = action.payload;
+        const existedItem = state.items.find(item => item.name == toBeRemoveItem.name);
+
+        if(existedItem) {
+            state.items = state.items.filter(item => item.name != toBeRemoveItem.name);
+        }
     },
-    updateQuantity: (state, action) => {
-const { name, quantity } = action.payload;
-const itemToUpdate = state.items.find(item => item.name === name);
-if (itemToUpdate) {
-  itemToUpdate.quantity = quantity;
-}  
+    updateQuantity; (state, action) => {
+        const toBeUpdatedItem  = action.payload;
+        const existedItem = state.items.find(item => item.name == toBeUpdatedItem.name);
+       
+        if(existedItem) {
+            existedItem.quantity = toBeUpdatedItem.quantity;
+    }  
     },
   },
 });
